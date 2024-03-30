27abaa5c44908bd95284a475b03689c570f55a64
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-30 17:52:06 +0000

    save changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,38 @@
-import "../style/index.css";
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
-function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
-}
-
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should show the cover image
-    includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
-  };
-  render(window.variables); // render the card for the first time
-
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
-    });
+  emailjs.init({
+    publicKey: "abmGTK_hTUm62lFTg"
   });
+
+  const button = document.querySelector("#send-info");
+  button.addEventListener("click", sendEmails);
 };
+
+async function sendEmails() {
+  const name = document.querySelector("#name-input").value;
+  const email = document.querySelector("#email-input").value;
+  const message = document.querySelector("#message-input").value;
+
+  const result = await emailjs.send("service_imi33v9", "template_tj7guv1", {
+    to_name: name,
+    to_email: email
+  });
+  console.log(result);
+
+  const result2 = await emailjs.send("service_imi33v9", "template_fpg57nn-", {
+    from_name: name,
+    from_email: email,
+    message: message
+  });
+  console.log(result2);
+
+  document.querySelector("#name-input").value = "";
+  document.querySelector("#email-input").value = "";
+  document.querySelector("#message-input").value = "";
+}
